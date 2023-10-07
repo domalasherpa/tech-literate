9a31798ca76eefe5e4a6097cb24cc6570e1696d5
domalasherpa <domalasherpa9813@gmail.com>
2023-10-07 11:11:38 +0545

    props

--- learningVue/js/components/AppButton.js
+++ learningVue/js/components/AppButton.js
@@ -1,7 +1,15 @@
 
 export default {
     template: `
-        <button class="bg-gray-200 hover:bg-gray-400 px-4 py-2 border rounded" v-model:disabled="processing">
+        <button 
+            :class="{
+                'px-4 py-2 border rounded': true,
+                'bg-blue-600 hover:bg-blue-700': type === 'primary',
+                'bg-gray-200 hover:bg-gray-400': type === 'secondary',
+                'bg-purple-200 hover:bg-purple-400': type === 'muted',
+                'is-loading': processing,
+            }" 
+            v-model:disabled="processing">
             <slot/> </button>
     `,
 
@@ -9,13 +17,12 @@ export default {
         type:{
             type: String,
             default: 'primary',
+        },
+
+        processing:{
+            type: Boolean,
+            default: false,
         }
     },
 
-    data(){
-        return{
-            'processing': false,
-        };
-    }
-    
 }
