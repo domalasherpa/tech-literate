7b2b63e25035200a266028983fd9fa539c8cb613
domalasherpa <domalasherpa9813@gmail.com>
2023-10-21 13:18:18 +0545

    numeric functions

---
+++ learningPython/operators/numeric.py
@@ -0,0 +1,15 @@
+
+a = [2, 3, 4, 5, 0, -1, -2]
+
+print(min(a))
+print(max(a))
+print(sorted(a))   #ascending order
+print(sorted(a, reverse=True))  #descending order
+print(sum(a))
+
+print(round(4.4, 0))
+print(type(round(4.4, 0)))  #gives float
+print(round(4.5))
+print(type(round(4.5)))     #gives int  
+print(round(4.6))
+print(round(4.45, 1))
