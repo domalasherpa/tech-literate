c35f2c68c2ac77a2f75970561a321502bbda6549
domalasherpa <domalasherpa9813@gmail.com>
2023-10-21 08:51:56 +0545

    basic string methods

---
+++ learningPython/string/stringMethods.py
@@ -0,0 +1,16 @@
+
+str = " Hello from D, from D other side "
+str2 =  "121221"
+print(str.lower())
+print(str.casefold())
+print(len(str))
+print(str.count("D"))
+print(str.endswith("side"))
+print(str.startswith("Hello"))
+print(str.find("Z"))
+print(str.find("D", 15, 31))
+print(str.index("D"))
+print(str.replace(',', "!"))
+print(str2.isnumeric())
+print(str.title())
+print(str.strip())
